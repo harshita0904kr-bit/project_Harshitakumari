8f31c0c80c75e1b73682ecef4d4cfa281cca28ee
harshita0904kr-bit <harshita0904kr@gmail.com>
2025-11-14 12:34:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,545 @@
+# # #x=5
+# # #y=2
+# # #print(x+y)
+# # #print(x-y)
+# # #print(x*y)
+# # #print(x/y)
+
+# # #print('Hello world')
+
+# # #x=8
+# # #y="harshita"
+# # #print(x)
+# # #print(y)
+
+# # #x=8
+# # #y="harshita"
+# # #print(type(x))
+# # #print(type(y))
+
+# x=5
+# y=4
+# print(bool(x+y))
+# print(bool(x+y))
+
+ # x=y=z="apple"
+  # print(x)
+  #print(y)
+ #print(z)
+
+# # #fruits=["apple","banana","cherry"]
+# # #x,y,z=fruits
+# # #x=y=z=fruits
+# # #fruits=[2,4,7,8,34,50]
+# # #print(x)
+# # ##print(y)
+# # #print(z)
+# # #print(fruits)
+# # #print(type(x))
+# # #print(fruits)
+# # #print(type(fruits))
+
+# # #x="This Is Python"
+# # #print(x)
+
+# # #x="python"
+# # #y="is"
+# # #z="good"
+# # #print(x,y,z)
+
+
+
+
+# # #x=5
+# # #y=6
+# # #print(x+y)
+
+# # #x="good"
+# # # global var
+# # #def my_function():
+# # function opening
+
+# #       print("Python is " + x)
+# #     # action
+
+# # #my_function()
+# function clossing
+
+
+# # #def my_function():
+# # #print("Python is " + x)
+# # #my_function
+
+
+# # # def my_function():
+# # #     global x
+# # #     x="good"
+# # # my_function()
+# # # print("Python is " + x)
+
+# x="good"
+# def my_function():
+#     print("Python is " + x)
+# my_function()
+
+
+# # #DataTypes
+
+# x=5
+# print(type(x))
+
+# x="name"
+# print(type(x))
+
+# x="5.6"
+# print(type(x))
+
+# x=5.6
+# print(type(x))
+
+# x=1j
+# print(type(x))
+
+# x={"apple","banana",}
+# print(type(x))
+
+# x="good"
+# print(range)
+
+
+# x=20
+# y=10
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x%y)
+# print(x**y)
+# print(x//y)
+
+# # list
+# list1=[1,2,4,5,6]
+# list1.append("apple")
+# print(list1)
+# list1.insert(2,"banana")
+# print(list1)
+# list1.remove("apple")
+# print(list1)
+# list1.pop()
+# print(list1)
+# list1.append(6)
+# print(list1)
+# list1.remove("banana")
+# print(list1)
+
+
+# my_list=["apple","banana"]
+# print(type(my_list))  
+
+# print(my_list)
+
+
+# print(len(my_list))
+
+
+# my_list=["apple","banana","cherry"]
+# print(my_list[1])
+
+# print(my_list[-1])
+
+
+# my_list=["apple","banana","cherry","sjc","bkdcsfg","vbajdo"]
+# print(my_list[2:5])
+
+# my_list=["apple","banana","cherry","sjc","bkdcsfg","vbajdo"]
+# print(my_list[-5:-2])
+
+# my_list=["apple","banana","cherry","sjc","bkdcsfg","vbajdo"]
+# my_list[1]="mango"
+# print(my_list)
+
+
+# text="hello Python"
+
+# print(text.upper())  #Output:HELLO PYTHON
+# print(text.lower())
+# print(text.strip())
+# print(text.replace("hello","world"))
+# print(text.split())
+
+
+
+
+# x=5
+# y="bdcbk"
+
+
+# #format String
+
+
+# Name="bob"
+# age=20
+# print(x+y)
+
+
+# x="bob"
+# y=20
+# print(f"my name is {x} and my age is {y}")
+
+
+# x=5
+# x+=3
+# print(x)
+
+# x-=3
+# print(x)
+# x*=3
+# print(x)
+# x/=3
+# print(x)
+
+
+
+# x=4
+# y=5
+# print(x==y)
+# print(x!=y)
+# print(x>=y)
+# print(x<=y)
+# print(x>y)
+# print(x<y)
+
+
+# x=8
+# y=x
+# print(y)
+
+# x=8
+# y=x
+# z=10
+# #print(y)
+# print(x is y)
+# print(x is not y)
+
+
+# colors={"red","green","blue"}
+# print(colors)
+
+
+# colors.add("yellow")
+# print(colors)
+
+
+# colors.remove("green")
+# print(colors)
+
+
+# for color in colors:
+#     print(color)
+
+
+# students={
+#     "name":"chandan",
+#     "age":30,
+#     "grades":"A"
+# }
+# print(students)
+
+
+
+# students["age"]=21
+# students["city"]="Delhi"
+# print(students)
+
+# students.pop("grade")
+# del students["city"]
+# print(students)
+
+
+# #if else
+
+
+
+
+# age=20
+
+# if age>=18:
+#     print("you are adult")
+# else:
+#     print("you are not adult")
+
+
+# age=20
+# if age<=18:
+#     print("you are adult")
+# else:
+#     print("you are not adult")
+
+# age=18
+# if age>18:
+#     print("you are adult")
+# elif age==18:
+#     print("you are teen")
+# elif age==17:
+#     print("you are 17")
+
+# elif age==16:
+#     print("you are 16")
+# else:
+#     print("you are not adult")
+    
+ #nasted if   
+    
+# x=15
+    
+# if x>10:
+#     print("x is greater then 10")
+        
+#     if x>20:
+#         print("x is greater then 20")
+#     else:
+#         print("x is not greater then 20") 
+
+# x=17
+# if x%2 ==0:
+#     print(f"even")
+# else:
+#     print(f"odd")
+    
+    
+    
+# fruits=["apple","banana","cherry"] 
+# print(fruits)
+          
+#loop
+          
+# word="python"
+# for letter in word:
+#     print(letter)
+
+
+
+# for i in range(5):
+#     print(i) 
+    
+# for i in range(1,10,2):   
+#      print(i)
+    
+# for i in range(1,4):
+#  print(i)
+
+
+# for i in range(1,4):
+#     for j in range(1,3):
+#         print(f"i={i},j={j}")
+
+
+
+# for i in range(1,6):
+#    if i==4:
+#        break
+#    print(i)
+   
+# for i in range(1,6): 
+#  if i==3:
+#     continue
+#  print(i)
+ 
+
+ 
+#  for i in range(1,4): 
+#       print(i)
+#  else  
+
+# #question  
+# for i in range(1,6):
+#     print(i) 
+    
+    
+# x=30
+# if x%2==0:
+#     print(f"even")  
+# else:
+#   print(f"odd")  
+  
+  
+# def greet():
+#       print("Hello,Python!")
+      
+#       greet() ##calling the function
+      
+      
+# def greet(name):
+#  print(f"Hello,{name}!") 
+       
+# greet("Alice")
+# greet("Bob")  
+     
+# def add(a,b):
+#     return a + b 
+# result=add(5,3)
+# print(result) 
+
+# def sub(a,b):
+#     return a - b
+# result=sub(5,3)
+# # print(result)
+
+# def mull(a,b):
+#     return a * b
+# result=mull(5,3)
+# print(result)
+
+# def div(a,b):
+#     return a / b
+# result=div(5,3)
+# print(result)
+
+# def greet(name="Student"):
+#     print(f"Hello,{name}!")  
+# greet()  #Hello,Student! 
+# greet("Alice")#Hello,Alice!   
+
+
+
+# x=10 # global
+# def my_func():
+#     y=5 # local
+#     print("inside:",x,y)
+    
+    
+#     my_func()
+#     print("Outside:",x)
+#     #print(y)->Error,y is local  
+
+# def my_func():
+#     print(x,y)
+# my_func()
+# print(x)
+# print(y)    
+
+# def greet():
+#     print(f"hello")
+# greet()
+
+
+# def add(a,b):
+#     return a+b
+# result=add(123,342)
+# print(result)
+
+# #Class
+# class Car:
+#   def __init__(self,brand,color):
+#     self.brand = brand 
+#     self.color = color
+    
+#   def drive(self):
+#       print(f"{self.color}{self.brand} is driving ")   
+      
+# #Object
+# car1 = Car("BMW","Black")
+# car2 = Car("Tesla","White")
+
+# car1.drive()
+# car2.drive()
+
+ 
+# #Array
+ 
+# import array
+
+
+
+#Create an array of intrgers
+# numbers=array.array('i',[1,2,3,4,5])
+# print(numbers)
+  
+# number=array.array[1,2,3,4,5]     
+# print(number)
+
+
+# from numpy import random
+
+# x=random.randint(100)
+# print(x)
+
+# x=random.rand()
+# print(x)
+# print(type(x))
+
+
+
+# from numpy import random
+# x=random.randint(100,size=(5))
+
+# print(x)
+
+# x=random.randint(100,size=(3,5))
+# print(x)
+
+
+
+# x=random.randint(100,size=(3,5,7))
+# print(x)
+
+# x=random.randint(100,size=(2,2,2,5))
+# print(x)
+
+
+# x=random.choice([4,5,6] , size=(5))
+# print(x)
+
+
+
+# x=random.choice([4,5,6] , size=(2,3,5))
+# print(x)
+
+
+
+# # # pandas
+
+# import pandas as pd
+
+# data=[10,20,30,40]
+# s=pd.Series(data)
+
+# print(s)
+
+
+# import pandas as pd
+
+# data={
+#     "Name":["Alice","Bob","Charlie","David"],
+#     "Age":[24,27,22,32],
+#     "City":["Delhi","Mumbai","Chennai","Kolkata"]
+# }
+
+
+# df=pd.DataFrame(data)
+
+
+# print(df)
+
+#From a NumPy Array
+
+
+# import numpy as np
+
+
+# arr = np.array([1,2,3,4,5])
+# s=pd.Series(arr)
+
+# print(s)
+
+#From a Dictionary
+
+# data={"Math":90,"Science":85,"English":78}
+
+# s=pd.Series(data)
+
+# print(s)
+
+import pandas as pd
+df = pd.read_csv("crocodile_dataset.csv")
+print(df.head())
